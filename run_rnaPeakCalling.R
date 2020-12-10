17cfb2f85fbffbfacf7ad143c109ebf39d97fa04
Tom Nicholson <nicholson.tom.n@gmail.com>
2020-12-10 13:14:07 +1300

    updating

--- run_rnaPeakCalling.R
+++ run_rnaPeakCalling.R
@@ -108,7 +108,7 @@ plotDatncRNA  <- tryCatch({
 plotDatncRNA$V1 <- plotDatncRNA$V1/total
 plotDatncRNA$V2 <- plotDatncRNA$V2/total
 print(15/total)
-q(status = 1)
+
 
 cat("Running rnPeakCalling\n")
 
