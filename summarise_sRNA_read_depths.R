00c38c438cf5d8ffa5e451613abe5dbc4855b201
Tom Nicholson <nicholson.tom.n@gmail.com>
2021-02-25 12:21:50 +1300

    large update

--- summarise_sRNA_read_depths.R
+++ summarise_sRNA_read_depths.R
@@ -8,8 +8,9 @@ dat <- read.table(args[1])
 valuesDat <- dat %>%
   summarise_all(list(max)) %>% as.matrix() %>% t()
 
+
 values <- valuesDat[,1]
-outDat <- data.frame(mean = mean(values), max  = max(values))
+outDat <- data.frame(mean = mean(values), max  = max(values), count = length(values[values > 0]))
 
 write.table(x = outDat, file = "~/phd/RNASeq/tmp/test.values", row.names = F, col.names = F, quote = F)
 
