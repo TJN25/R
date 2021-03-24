98e9534f9b23be7baa0f4f0cc6f08f07a50649e6
Tom Nicholson <nicholson.tom.n@gmail.com>
2021-03-24 17:16:56 +1300

    update

--- examples_heatmaps.R
+++ examples_heatmaps.R
@@ -12,12 +12,12 @@ outname <- "GCA_000222975.1_486"
 filepath <- "~/phd/RNASeq/examples"
 
 
-# id <- args[1]
-# outname <- args[2]
-# filepath <- args[3]
+id <- args[1]
+outname <- args[2]
+filepath <- args[3]
 
-for(outname in c("GCA_000015425.1_257", "GCA_000213655.1_267", "GCA_000196795.1_104", "GCA_000015425.1_172", "GCA_000438825.1_154",
-                 "GCA_000438825.1_15", "GCA_000222975.1_486", "GCA_002850215.1_141")){
+# for(outname in c("GCA_000015425.1_257", "GCA_000213655.1_267", "GCA_000196795.1_104", "GCA_000015425.1_172", "GCA_000438825.1_154",
+#                  "GCA_000438825.1_15", "GCA_000222975.1_486", "GCA_002850215.1_141")){
 
 dat <- read.table(paste(filepath, "/", outname, "_example_files/", outname,"_read_values.txt", sep = ""), as.is = T, stringsAsFactors = F, fill =T)
 
@@ -64,14 +64,14 @@ p <- ggplot() +
   scale_fill_gradient(low = 'white', high = 'red') +
   ggtitle(outname)
 # print(p)
-# ggsave(filename = paste(filepath, "/heatmaps/", outname,"_read_depths_heatmap_log.svg", sep = ""), plot = p, width = 450, height = 450, units = "mm")
-print(outname)
+ggsave(filename = paste(filepath, "/heatmaps/", outname,"_read_depths_heatmap_log.svg", sep = ""), plot = p, width = 450, height = 450, units = "mm")
+# print(outname)
 print(cols)
-}
-
-df <- data.frame(a = c(1,5,10,50,100,500,1000,5000), b = c(1,2,3,4,5,6,7,8), c = c(1,1,1,1,1,1,1,1))
-ggplot() +
-  geom_tile(data =df, aes(x = b, y = c, fill = log10(a))) +
-  scale_fill_gradient(low = 'white', high = 'red') 
-
-log10(df$a)
+# }
+# 
+# df <- data.frame(a = c(1,5,10,50,100,500,1000,5000), b = c(1,2,3,4,5,6,7,8), c = c(1,1,1,1,1,1,1,1))
+# ggplot() +
+#   geom_tile(data =df, aes(x = b, y = c, fill = log10(a))) +
+#   scale_fill_gradient(low = 'white', high = 'red') 
+# 
+# log10(df$a)
