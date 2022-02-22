082c65a009088d8f8d84ad4d5cf20515b4db9465
TJN25 <nicholson.tom.n@gmail.com>
2022-02-23 07:47:02 +1300

    a couple of updates to fix small things. new files added

--- RNA-Seq_experiments_counts.R
+++ RNA-Seq_experiments_counts.R
@@ -7,8 +7,9 @@ library(tidyverse)
 #   each genus contained at least 2 genomes after filtering (this was ignored 
 #     for a couple of genomes to fill in a large taxonomic gap)
 
-load('r_files/SRA_bacteria_RNASeq_v2_all.Rda')
 
+
+#take the input data and filter based on criteria
 filterData <- function(dat){
   dat <- dat %>% filter(grepl(pattern = 'RNA-Seq', x = Assay.Type, ignore.case = T),
                         grepl(pattern = 'lumina', x= Instrument, ignore.case = T), 
@@ -16,6 +17,7 @@ filterData <- function(dat){
   return(dat)
 }
 
+#count the number of experiments and genomes found
 getGenusCounts <- function(dat){
   dat <- dat %>% separate(col = Organism, into = c("genus"), sep = " ", extra = 'drop', remove = F)
   dat <- dat %>% group_by(Organism, genus) %>% summarise(experiments.count = n()) %>% 
@@ -26,9 +28,196 @@ getGenusCounts <- function(dat){
   return(returnList)
 }
 
+#the columns
+getGenusColumns <- function(dat, genus_names){
+  genus_vals <- list(rows = rep(NA, nrow(dat)), 
+                     cols = rep(NA, nrow(dat)), 
+                     organism = rep(NA, nrow(dat)),
+                     run = rep(NA, nrow(dat)),
+                     layout = rep(NA, nrow(dat)),
+                     instrument = rep(NA, nrow(dat)))
+  dat <- dat %>% mutate_all(as.character)
+  for(row in 1:nrow(dat)){
+    print(row)
+    for(name in genus_names){
+      for(col in 1:ncol(dat)){
+        if(is.na(dat[row, col])){
+          next
+        }
+        layout_found <-  'PAIRED' == dat[row, col] | 'SINGLE' == dat[row, col]
+        if(layout_found){
+          genus_vals$layout[row] <- dat[row, col]
+          next
+        }
+        instrument_found <- grepl(pattern = "Illumina", x = dat[row, col], ignore.case = T)
+        if(instrument_found){
+          genus_vals$instrument[row] <- dat[row, col]
+          next
+        }
+        name_found <- grepl(pattern = name, x = dat[row, col])
+        if(name_found){
+          genus_vals$rows[row] <- row
+          genus_vals$cols[row] <- col
+          genus_vals$run[row] <- dat[row, 1]
+          genus_vals$organism[row] <- name
+          next
+        }
+        
+      }
+    }
+  }
+  
+  return(genus_vals)
+}
+
+load('r_files/SRA_bacteria_RNASeq_v2_all.Rda')
+
+genus_names <- c("Escherichia", "Shigella", "Salmonella", "Enterobacter", "Klebsiella",
+                 "Plautia", 'Citrobacter', 'Serratia', 'Wigglesworthia', 'Buchnera', 
+                 'Sodalis', 'Lonsdelea', 'Brenneria', 'Dickeya', 'Edwardsiella', 
+                 'Pantoea', 'Erwinia', 'Proteus', 'Providencia', 'Xenorhabdus', 
+                 'Photorhabdus', 'Mannheimia', 'Aggregatibacter', 'Alishewanella', 
+                 'Acinetobacillus', 'Yersinia', 'Vibrio', 'Alteromonas', 'Agarivoranas', 
+                 'Pseudoalteromonas', 'Moritella', 'Shewanella', 'Psychrobacter', 
+                 'Moraxella', 'Acinetobacter', 'Pseudomonas', 'Marinobacter', 'Methylomicrobium', 
+                 'Methyloccocus', 'Francisella', 'Pseudoxanthomonas', 'Stenotrophomonas', 
+                 'Xanthomonas', 'Xylella', 'Lysobacter')
+
+
+if(FALSE){
 
 sraFiltered <- filterData(SRA_bacteria_RNASeq_v2_all)
 returnList <- getGenusCounts(sraFiltered)
 
 experimentsCounts <- returnList[[1]]
 genomeCounts <- returnList[[2]]
+
+#this has been taken out of a function
+dat <- SRA_bacteria_RNASeq_v2_all
+genus_vals <- list(rows = rep(NA, nrow(dat)), 
+                   cols = rep(NA, nrow(dat)), 
+                   organism = rep(NA, nrow(dat)),
+                   run = rep(NA, nrow(dat)),
+                   layout = rep(NA, nrow(dat)),
+                   instrument = rep(NA, nrow(dat)))
+dat <- dat %>% mutate_all(as.character)
+for(row in 1:nrow(dat)){
+  genus_vals$run[row] <- dat[row, 1]
+  for(name in genus_names){
+    for(col in 1:ncol(dat)){
+      if(is.na(dat[row, col])){
+        next
+      }
+      layout_found <-  'PAIRED' == dat[row, col] | 'SINGLE' == dat[row, col]
+      if(layout_found){
+        genus_vals$layout[row] <- dat[row, col]
+        next
+      }
+      instrument_found <- grepl(pattern = "Illumina", x = dat[row, col], ignore.case = T)
+      if(instrument_found){
+        genus_vals$instrument[row] <- dat[row, col]
+        next
+      }
+      name_found <- grepl(pattern = name, x = dat[row, col])
+      if(name_found){
+        genus_vals$rows[row] <- row
+        genus_vals$cols[row] <- col
+        genus_vals$organism[row] <- dat[row, col]
+        next
+      }
+      
+    }
+  }
+}
+
+
+
+
+
+
+check_rows <- as.numeric(row.names(genus_dat[is.na(genus_dat$rows),]))
+
+
+
+for(i in check_rows){
+  print(i)
+  genus_vals$run[row] <- dat[row, 1]
+  for(name in genus_names){
+    for(col in 1:ncol(dat)){
+      if(is.na(dat[row, col])){
+        next
+      }
+      layout_found <-  'PAIRED' == dat[row, col] | 'SINGLE' == dat[row, col]
+      if(layout_found){
+        genus_vals$layout[row] <- dat[row, col]
+        next
+      }
+      instrument_found <- grepl(pattern = "Illumina", x = dat[row, col], ignore.case = T)
+      if(instrument_found){
+        genus_vals$instrument[row] <- dat[row, col]
+        next
+      }
+      name_found <- grepl(pattern = name, x = dat[row, col])
+      if(name_found){
+        genus_vals$rows[row] <- row
+        genus_vals$cols[row] <- col
+        genus_vals$organism[row] <- dat[row, col]
+        next
+      }
+      
+    }
+  }
+}
+
+save(genus_vals, file = 'r_files/rnaseq_experiments_list_all.Rda')
+}
+## Working from here!
+
+load('r_files/rnaseq_experiments_list_all.Rda')
+load('r_files/SRA_bacteria_RNASeq_v2_all.Rda')
+dat <- SRA_bacteria_RNASeq_v2_all %>% mutate_all(as.character)
+
+# originally had the function call genus_vals <- getGenusColumns(SRA_bacteria_RNASeq_v2_all, genus_names)
+# this failed so took out the loop so that I can restart mid loop
+# should really learn a better way of writing this.
+
+
+plot_files <- read.table('~/bin/PhD/Chapter_4/chapter_4_files/plot_file_names.txt')
+colnames(plot_files) <- c('genus.name', 'genome.accession', 'run')
+
+genus_dat <- as.data.frame(genus_vals)
+
+genus_dat <-  genus_dat %>% full_join(plot_files, by = 'run')
+
+genus_dat <- genus_dat[!is.na(genus_dat$rows) | !is.na(genus_dat$genome.accession),]
+
+for(i in 1:nrow(genus_dat)){
+  if(is.na(genus_dat$rows[i]) | is.na(genus_dat$cols[i])){
+    next
+  }
+  genus_dat$organism[i] <- dat[genus_dat$rows[i], genus_dat$cols[i]]
+}
+
+
+
+
+filtered_dat <- genus_dat[genus_dat$layout == 'PAIRED' | is.na(genus_dat$layout),]
+filtered_dat <- filtered_dat[!is.na(filtered_dat$rows) | !is.na(filtered_dat$genome.accession),]
+
+
+# next step ---------------------------------------------------------------
+
+#Moraxella not working
+
+#there appear to be a number of runs missing
+#some are labelled as experiments, 
+#some will be from the original dataset
+#need to track them all down
+
+
+x  <- c(1:10)
+y  <- x^2
+
+ggplot() +
+  geom_point(aes(x, y), color = "Red")
+
---
+++ functions.R
@@ -0,0 +1,270 @@
+###Should list the required packages here
+
+#take a named vector and convert to a data frame with the names turned into a column
+vector_to_dataframe <- function(vec, new_col = 'V2'){
+  vec <- as.data.frame(vec) 
+  vec[[new_col]] <- row.names(vec)
+  rownames(vec) <- NULL
+  return(vec)
+}
+
+#select and order columns of a data frame using a vector of column names
+select_columns_by_list <- function(dat, vec){
+  dat <- dat[, colnames(dat) %in% vec]
+  dat <- dat[, match(vec, colnames(dat))]
+  return(dat)
+}
+
+#set first letter to capital
+firstup <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
+
+#take an input, and calculate the cumulative counts of rows sorted by the target column
+cumulativeCounts <- function(dists, smooth = T, target_column = 'max_dist'){
+  groups <- unique(dists$group)
+  for(i in groups){
+    dat <- dists %>% filter(group == i)
+    dat <- dat %>% mutate(count = 1) %>%
+      arrange(-.data[[target_column]]) %>% group_by(group) %>%
+      mutate(cumulativeCount = cumsum(count)) %>% ungroup() %>%
+      group_by(group, .data[[target_column]]) %>% summarise(cumulative_prop = max(cumulativeCount)/ nrow(dat))
+    if(smooth){
+      dat <- as.data.frame(spline(x = dat[[target_column]],y =  dat$cumulative_prop))
+    }
+    dat <- dat %>% ungroup() %>% mutate(group = i)
+    if(exists('combinedDat')){
+      combinedDat <- combinedDat %>% bind_rows(dat)
+    }else{
+      combinedDat <- dat
+    }
+  }
+  combinedDat$group <- factor(combinedDat$group, levels = c('low', 'Positive Control', 'high'))
+  return(combinedDat)
+}
+
+#plot the output of the cumulative counts and run a K-S test
+cumulativeDistribution <- function(dat, run.ks.test = F, alternative_pred, alternative_pc, alternative = 'two.sided', show.legend = F, target_column = 'max_dist'){
+  if(missing('alternative_pred')){alternative_pred <- alternative}
+  if(missing('alternative_pc')){alternative_pc <- alternative}
+  distsCumulativeCount <- cumulativeCounts(dists = dat, smooth = F, target_column = target_column)
+  distsCumulativeCount <- distsCumulativeCount %>% filter(group != "Predicted Known")
+  
+  p <- ggplot() +
+    geom_line(data = distsCumulativeCount, aes(x= .data[[target_column]], y = cumulative_prop, group = group, colour = group), size = 1, show.legend = show.legend) + theme_classic()
+  
+  if(run.ks.test == T){
+    pos <- dat %>% filter(group == "Positive Control")
+    neg <- dat %>% filter(group == "Negative Control")
+    pred <- dat %>% filter(group == "Predicted")
+    res <- ks.test(x = pred[[target_column]], y = neg[[target_column]], alternative = alternative_pred)
+    print(res)
+    res <- ks.test(x = pos[[target_column]], y = neg[[target_column]], alternative = alternative_pc)
+    print(res)
+  }
+  return(p)
+}
+
+#set up the data sets for later merging
+readDepthsSetup <- function(file_path){
+  readsColName <- c("mean.val", "max.val", "counts.above.threshold", "ID", "genus")
+  reads.dat <- read.table(file_path, as.is = T, stringsAsFactors = F)
+  colnames(reads.dat) <- readsColName
+  reads.max <- reads.dat  %>% group_by(ID) %>% summarise(read.max.score  =  sum(max.val))
+  reads.mean <- reads.dat %>%
+    group_by(ID) %>%
+    summarise(reads.mean.score  = sum(mean.val))
+  reads.count <- reads.dat %>%
+    group_by(ID) %>%
+    filter(max.val > 0) %>%
+    summarise(max.val = max(max.val), read.counts  = n()) %>% 
+    select(-max.val)
+  reads.dat <- reads.max %>% full_join(reads.mean, by = "ID")
+  reads.dat <- reads.dat %>% full_join(reads.count, by = "ID")
+  return(reads.dat)
+}
+
+rscapeCovarianceSetup <- function(file_path){
+  cov.dat <- read.table(file_path, sep = "\t", comment.char = "#", as.is = T, header = F, fill = T, col.names = c("V1", "left_pos", "right_pos", "score", "e.value", "substitutions", "V2", "power", "ID"))
+  cov.dat <- cov.dat %>% select(ID, score, e.value, power)
+  cov.mean <- cov.dat %>% group_by(ID) %>% summarise(cov.mean.score = max(score))
+  cov.count <- cov.dat %>% group_by(ID) %>% summarise(cov.count = n())
+  cov.max <- cov.dat %>% group_by(ID) %>% summarise(cov.min.eval = min(e.value))
+  cov.power <- cov.dat %>% group_by(ID) %>% summarise(power = sum(power))
+  cov.dat <- cov.mean %>% full_join(cov.max, by = "ID") %>%
+    full_join(cov.count, by = "ID") %>%
+    full_join(cov.power, by = "ID")
+  cov.dat <- cov.dat %>% filter(!is.na(cov.mean.score)) %>% mutate(cov.combined.score = cov.count * cov.mean.score) 
+  return(cov.dat)
+}
+
+gcSetup <- function(file_path){
+  dat <- read.table(file_path)
+  colnames(dat) <- c("counts", "letter", "ID")
+  
+  datTotals <- dat %>% group_by(ID) %>% summarise(total = sum(counts))
+  
+  dat <- dat %>% filter(letter %in% c("C", "G")) %>% group_by(ID) %>% summarise(gc.count = sum(counts)) %>% left_join(datTotals, by = "ID") %>% 
+    mutate(gc.score = (gc.count/total)*100) %>% select(ID, gc.score)
+  
+  return(dat)
+}
+
+alifoldSetup <- function(file_path){
+  dat<- read.table(file_path, header = F, comment.char = "#", quote = "", sep = "",   fill = T, as.is = T, col.names = c( "From",      "To",    "Strand",    "Native.MFE",    "Mean.MFE",     "STDV",        "Z", "ID"))
+  
+  dat <- dat %>% filter(ID != "")
+  
+  datSD <- dat %>% select(ID, STDV)
+  datMean <- dat %>% group_by(ID) %>% summarise(z_mean = mean(as.numeric(Z), na.rm = T))
+  datMax <- dat %>% group_by(ID) %>% summarise(z_max = max(as.numeric(Z), na.rm = T))
+  dat <- datMean %>% full_join(datMax, by = "ID") %>% full_join(datSD, by = "ID") %>% 
+    filter(!is.nan(STDV)) %>% select(-STDV) %>% unique()
+  
+  return(dat)
+  
+}
+
+motifSetup <- function(file_path){
+  dat <- read.table(file_path, sep = "", comment.char = "#", as.is = T, header = F, fill = T)
+  
+  colnames(dat) <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute", "ID")
+  dat <- dat %>% group_by(feature, start, end, strand, ID) %>% summarise(score = max(score))
+  
+  
+  
+  datMean <- dat %>% group_by(ID) %>% summarise(motif.mean.score = mean(score))
+  datMax <- dat %>% group_by(ID) %>% summarise(motif.max.score = max(score))
+  datCount <- dat %>% group_by(ID) %>% summarise(motif_count = n())
+  
+  dat <- datMean %>% full_join(datMax, by = "ID") %>% 
+    full_join(datCount, by = "ID")
+  return(dat)
+}
+
+#replace the NAs with suitable null values to keep rows for random forest
+replaceNAs <- function(dat){
+  
+  dat$mfe.score[is.na(dat$mfe.score)] <- 0
+  dat$gc.score[is.na(dat$gc.score)] <- 50
+  dat$distance[is.na(dat$distance)] <- 0
+  dat$reads.mean.score[is.na(dat$reads.mean.score)] <- 0
+  dat$read.max.score[is.na(dat$read.max.score)] <- 0
+  dat$cov.mean.score[is.na(dat$cov.mean.score)] <- 0
+  dat$cov.min.eval[is.na(dat$cov.min.eval)] <- 10
+  # dat$cov.combined.score[is.na(dat$cov.combined.score)] <- 0
+  dat$cov.count[is.na(dat$cov.count)] <- 1
+  dat$read.counts[is.na(dat$read.counts)] <- 0
+  dat$motif.mean.score[is.na(dat$motif.mean.score)] <- 0
+  dat$motif.max.score[is.na(dat$motif.max.score)] <- 0
+  dat$motif_count[is.na(dat$motif_count)] <- 0
+  dat$z_mean[is.na(dat$z_mean)] <- 10
+  dat$z_max[is.na(dat$z_max)] <- 10
+  dat <- dat[dat$z_max != -Inf,]
+  dat$alifold.score[is.na(dat$alifold.score)] <- 0
+  dat$alifold_cov_score[is.na(dat$alifold_cov_score)] <- 0
+  # dat$alifold_cov_score[dat$alifold_cov_score > 0] <- 0
+  return(dat)
+}
+
+#calculate statistics for a dataset
+scoreProbabities <- function(probDat, threshold, target_column = 'max_dist'){
+  
+  tp <- probDat %>% filter(group == "Positive Control", .data[[target_column]] > threshold) %>% nrow()
+  fp <- probDat %>% filter(group == "Negative Control", .data[[target_column]] > threshold) %>% nrow()
+  pos <- probDat %>% filter(group == "Positive Control") %>% nrow()
+  pred_pos <- probDat %>% filter(group == "Predicted", .data[[target_column]] > threshold) %>% nrow()
+  pred_neg <- probDat %>% filter(group == "Predicted", .data[[target_column]] <= threshold) %>% nrow()
+  pred <- probDat %>% filter(group == "Predicted") %>% nrow()
+  
+  
+  tn <- probDat %>% filter(group == "Negative Control", .data[[target_column]] <= threshold) %>% nrow()
+  fn <- probDat %>% filter(group == "Positive Control", .data[[target_column]] <= threshold) %>% nrow()
+  neg <- probDat %>% filter(group == "Negative Control") %>% nrow()
+  
+  sens <- tp/pos ##sensitivity
+  spec <- tn/neg ##specificity
+  ppv <- tp/(tp+fp) ##positive predictive value
+  fpr <- fp/neg
+  fnr <- fn/pos
+  pred_pct <- pred_pos/pred
+  pc_pct <- tp/pos
+  nc_pct <- fp/neg
+  
+  res <- probDat %>% group_by(group) %>% summarise(positive_res = round(mean(.data[[target_column]] > threshold), 3)) %>% mutate(negative_res = 1 - positive_res)
+  
+  outList <- list()
+  outList$sensitivity <- sens
+  outList$specificity <- spec
+  outList$ppv <- ppv
+  outList$fpr <- fpr
+  outList$fnr <- fnr
+  outList$tp <- tp
+  outList$fp <- fp
+  outList$pred_pos <- pred_pos
+  outList$pred_neg <- pred_neg
+  outList$tn <- tn
+  outList$fn <- fn
+  outList$neg <- neg
+  outList$pred <- pred
+  outList$pred_pct <- pred_pct
+  outList$pc_pct <- pc_pct
+  outList$nc_pct <- nc_pct
+  outList$res <- res
+  return(outList)
+}
+
+
+printListSubset <- function(inList, vec = NA, startText = NA, getNames = F, round_val = 10){
+  if(getNames){
+    outNames <- "c('"
+    for(value in names(scores)){
+      outNames <- paste0(outNames, value, "', '")
+    }
+    outNames <- paste0(outNames, "')\n")
+    cat(outNames)
+  }else{
+  outputPrint = ''
+  if(!is.na(startText)){
+    outputPrint = paste0(startText, '\n  ')
+  }
+  
+  for(value in vec){
+      if(value %in% names(inList)){
+        outputPrint = paste0(outputPrint, value, ': ', round(inList[[value]], round_val), '\n  ')
+      }else{
+        print(paste0(value, 'not found'))
+      }
+  }
+  cat(outputPrint)
+  }
+}
+
+
+allScores <- function(dat, steps, target_column = 'max_dist'){
+  valuesDat <- data.frame(threshold = seq((0-steps),max(dat[[target_column]]), by=steps),
+                          ppv = NA, 
+                          fpr = NA,
+                          fnr = NA,
+                          pred_pct = NA,
+                          pc_pct = NA,
+                          pred_pos = NA,
+                          sensitivity = NA,
+                          specificity = NA
+                          )
+  counter <- 0
+  for(i in seq((0-steps),max(dat[[target_column]]), by=steps)){
+    counter <-  counter + 1
+    scores <- scoreProbabities(dat, i, target_column)
+    valuesDat$ppv[counter] <- scores$ppv 
+    valuesDat$fpr[counter] <- scores$fpr 
+    valuesDat$fnr[counter] <- scores$fnr 
+    valuesDat$pred_pct[counter] <- scores$pred_pct 
+    valuesDat$pc_pct[counter] <- scores$pc_pct 
+    valuesDat$pred_pos[counter]<- scores$pred_pos 
+    valuesDat$sensitivity[counter]<- scores$sensitivity 
+    valuesDat$specificity[counter]<- scores$specificity 
+  }
+  return(valuesDat)
+}
